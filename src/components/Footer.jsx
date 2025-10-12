f366ab6c018b1e1fb46545b05ade73c06792e038
Ümit Arat <umitarat8098@gmail.com>
2025-10-13 01:22:02 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <div className="fixed-bottom text-center bg-light p-3">
+        Copyright © 2025 UmitDev
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/pages/Instructors-useLocationn.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Instructors = () => {
+  
+  const navigate = useNavigate();
+
+  const [instructors, setInstructors] = useState([]);
+
+  const getInstructors = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => setInstructors(data))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getInstructors();
+  }, []);
+
+  return (
+    <div className="container text-center mt-4">
+      <h1>INSTRUCTOR LIST</h1>
+      <div className="row justify-content-center g-4">
+        {instructors?.map((inst) => {
+          const { id, name } = inst;
+          return (
+            
+            <div
+              className="col-sm-12 col-md-6 col-lg-4"
+              key={id}
+              
+              // onClick={() => navigate(`/instructors/${id}`)}
+
+              //! Relative path - useLocation() 
+              onClick={() => navigate(`${id}`, { state: inst })}
+              
+              //! Absolute path - useLocation()
+              // onClick={() => navigate(`/instructors/${id}`, { state: inst })}
+              style={{ cursor: 'pointer' }}
+            >
+              <img
+                // src={`https://i.pravatar.cc/300/${id}`}
+                src={`https://i.pravatar.cc/300?img=${id}`}
+                alt=""
+              />              
+              <h6>{name}</h6>
+            </div>
+
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Instructors;
